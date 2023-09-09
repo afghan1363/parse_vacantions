2386f4e0c158f10e811c3b2d094cffb120dfb33e
Maxim Abavi <afghan1363@ya.ru>
2023-09-10 03:34:44 +0500

    класс для работы с вакансиями

---
+++ classes/vacancies.py
@@ -0,0 +1,9 @@
+class Vacancy:
+    def __init__(self, title, desc, salary_from, salary_to, url, employer):
+        self.title = title
+        self.desc = desc
+        self.salary_from = salary_from
+        self.salary_to = salary_to
+        self.url = url
+        self.employer = employer
+
