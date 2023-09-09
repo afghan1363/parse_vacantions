ac9bad24a3eaa819c7f1ba36ef8cbc93181bd1d0
Maxim Abavi <afghan1363@ya.ru>
2023-09-10 03:31:09 +0500

    класс headhunter

--- classes/headhunter.py
+++ classes/headhunter.py
@@ -1,25 +1,44 @@
+import json
 import requests
-
 from classes.abstractclass import GetVacancies
+from classes.vacancies import Vacancy
 
 
 class HeadHunter(GetVacancies):
 
-    def get_vacancies(self, name, keyword, salary=None):
+    def __init__(self, search_query, salary=None, show_salary=False):
+        self.search_query = search_query
+        self.salary = salary
+        self.show_salary = show_salary
+
+    def get_vacancies(self):
         params = {
-            'text': 'NAME:Python',  # Текст фильтра. В имени должно быть слово "Аналитик"
-            'area': 113,  # Поиск ощуществляется по вакансиям города Москва
-            'page': page,  # Индекс страницы поиска на HH
+            'text': self.search_query,  # Текст фильтра.
+            'area': 113,  # Поиск осуществляется по вакансиям России
             'per_page': 100,  # Кол-во вакансий на 1 странице
-            'salary_from': 80000,
-            'only_with_salary': 'true',
-            'currency': 'RUR'
+            'salary_from': self.salary,
+            'only_with_salary': self.show_salary,
+            'currency': 'RUB'
         }
 
         req = requests.get('https://api.hh.ru/vacancies', params)  # Посылаем запрос к API
         data = req.content.decode()  # Декодируем его ответ, чтобы Кириллица отображалась корректно
         req.close()
-        return data
+        hh_json = json.loads(data)
+        return hh_json
 
-    def format_vacancies(self, keyword, salary=None):
-        pass
+    def format_vacancies(self):
+        vac = []
+        data = self.get_vacancies()
+        for el in data['items']:
+            if not el['salary']:
+                el['salary'] = {}
+            vac.append(Vacancy(
+                title=el['name'],
+                desc=el['snippet']['responsibility'],
+                salary_from=el['salary'].get('from', 'Не указано'),
+                salary_to=el['salary'].get('to', 'Не указано'),
+                url=el['apply_alternate_url'],
+                employer=el['employer']['name']
+            ))
+            return vac
