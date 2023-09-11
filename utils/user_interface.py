6b64c57d63692034ff0aab065cb88bdcabcb58f0
Maxim Abavi <afghan1363@ya.ru>
2023-09-12 04:24:30 +0500

    мелкие исправления

--- utils/user_interface.py
+++ utils/user_interface.py
@@ -16,27 +16,31 @@ def user_interface():
                        "1 - HeadHunter, 2 - SuperJob 3 - HeadHunter&SuperJob 4 - Выход:\n")
         if choice == '1':
             search_query = input("Введите ключевое слово вакансии: ")
-            salary = int(input("Можете ввести желаемый уровень зарплаты "))
-            show_salary = int(input("0 - показать все вакансии"
-                                    " 1 - показать только вакансии с указанной зарплатой "))
-            hh_api = HeadHunter(search_query=search_query, salary=salary, show_salary=bool(show_salary))
+            salary = input("Можете ввести желаемый уровень зарплаты ")
+            if salary.isalpha():
+                salary = input("Можете ввести желаемый уровень зарплаты ")
+            show_salary = input("0 - показать все вакансии"
+                                " 1 - показать только вакансии с указанной зарплатой ")
+            hh_api = HeadHunter(search_query=search_query, salary=salary, show_salary=bool(int(show_salary)))
             get_hh = hh_api.get_vacancies()
             format_vacancies_hh(get_hh)
             vacancies = Vacancy.all_vacancies
             json_editor.add_vacancy(vacancies)
-            top_n = int(input("Вы бери число ТОП вакансий по минимальной зарплате "))
-            sort_vacancies_by_salary(vacancies, top_n)
             if len(vacancies) == 0:
                 print("Такой вакансии нет")
             else:
-                top_n = int(input("Вы бери число ТОП вакансий по минимальной зарплате "))
-                sort_vacancies_by_salary(vacancies, top_n)
+                top_n = input("Выберите число ТОП вакансий по минимальной зарплате ")
+                if top_n.isalpha():
+                    top_n = input("Выберите число ТОП вакансий по минимальной зарплате ")
+                sort_vacancies_by_salary(vacancies, int(top_n))
         elif choice == '2':
             search_query = input("Введите ключевое слово вакансии: ")
-            salary = int(input("Можете ввести желаемый уровень зарплаты "))
-            show_salary = int(input("0 - показать все вакансии"
-                                    "1 - показать только вакансии с указанной зарплатой "))
-            sj_api = SuperJob(search_query=search_query, salary=salary, show_salary=show_salary)
+            salary = input("Можете ввести желаемый уровень зарплаты ")
+            if salary.isalpha():
+                salary = input("Можете ввести желаемый уровень зарплаты ")
+            show_salary = input("0 - показать все вакансии"
+                                " 1 - показать только вакансии с указанной зарплатой ")
+            sj_api = SuperJob(search_query=search_query, salary=salary, show_salary=int(show_salary))
             get_sj = sj_api.get_vacancies()
             format_vacancies_sj(get_sj)
             vacancies = Vacancy.all_vacancies
@@ -44,29 +48,35 @@ def user_interface():
             if len(vacancies) == 0:
                 print("Такой вакансии нет")
             else:
-                top_n = int(input("Вы бери число ТОП вакансий по минимальной зарплате "))
-                sort_vacancies_by_salary(vacancies, top_n)
+                top_n = input("Выберите число ТОП вакансий по минимальной зарплате ")
+                if top_n.isalpha():
+                    top_n = input("Выберите число ТОП вакансий по минимальной зарплате ")
+                sort_vacancies_by_salary(vacancies, int(top_n))
 
         elif choice == '3':
             search_query = input("Введите ключевое слово вакансии: ")
-            salary = int(input("Можете ввести желаемый уровень зарплаты "))
-            show_salary = int(input("0 - показать все вакансии"
-                                    "1 - показать только вакансии с указанной зарплатой "))
-            sj_api = SuperJob(search_query=search_query, salary=salary, show_salary=show_salary)
+            salary = input("Можете ввести желаемый уровень зарплаты ")
+            if salary.isalpha():
+                salary = input("Можете ввести желаемый уровень зарплаты ")
+            show_salary = input("0 - показать все вакансии"
+                                " 1 - показать только вакансии с указанной зарплатой ")
+            sj_api = SuperJob(search_query=search_query, salary=salary, show_salary=int(show_salary))
             get_sj = sj_api.get_vacancies()
             format_vacancies_sj(get_sj)
-            hh_api = HeadHunter(search_query=search_query, salary=salary, show_salary=bool(show_salary))
+            hh_api = HeadHunter(search_query=search_query, salary=salary, show_salary=bool(int(show_salary)))
             get_hh = hh_api.get_vacancies()
             format_vacancies_hh(get_hh)
             vacancies = Vacancy.all_vacancies
             json_editor.add_vacancy(vacancies)
             if len(vacancies) == 0:
-                print("Такой вакансии нет")
+                print("Такой вакансии нет.")
             else:
-                top_n = int(input("Вы бери число ТОП вакансий по минимальной зарплате "))
-                sort_vacancies_by_salary(vacancies, top_n)
+                top_n = input("Выберите число ТОП вакансий по минимальной зарплате ")
+                if top_n.isalpha():
+                    top_n = input("Выберите число ТОП вакансий по минимальной зарплате ")
+                sort_vacancies_by_salary(vacancies, int(top_n))
         elif choice == '4':
-            print("Удачного дня")
+            print("Удачного дня!")
             break
         else:
             print("Ошибочный запрос!")
@@ -87,6 +97,7 @@ def sort_vacancies_by_salary(vacancies, top_n):
         :return: ключ
         """
         return key["salary_from"]
+
     salary_from_digital = []
     for vacancy in vacancies:
         if vacancy['salary_from'] is not None:
@@ -94,7 +105,7 @@ def sort_vacancies_by_salary(vacancies, top_n):
     salary_from_digital.sort(key=key_to_sort, reverse=True)
     if top_n > len(salary_from_digital):
         top_n = len(salary_from_digital)
-        print("Число вакансий для вывода меньше запрашиваемых")
+        print("Число вакансий для вывода меньше запрашиваемого!")
     sorted_vacancies = salary_from_digital[:top_n]
     for vacancy in sorted_vacancies:
         print(f"""Вакансия {vacancy['title']}
