67c6fd36836eface334643f46a992b6bfae17c3a
Maxim Abavi <afghan1363@ya.ru>
2023-09-11 22:56:26 +0500

    удаление лишнего

--- utils/format_vacancies.py
+++ utils/format_vacancies.py
@@ -2,31 +2,26 @@ from classes.vacancies import Vacancy
 
 
 def format_vacancies_hh(data):
-    vac = []
     for el in data['items']:
         if not el['salary']:
             el['salary'] = {}
-        title = el['name'],
-        desc = el['snippet']['responsibility'],
-        salary_from = el['salary'].get('from', 'Не указано'),
-        salary_to = el['salary'].get('to', 'Не указано'),
-        url = el['apply_alternate_url'],
+        title = el['name']
+        desc = el['snippet']['responsibility']
+        salary_from = el['salary'].get('from', 0)
+        salary_to = el['salary'].get('to', 0)
+        url = el['apply_alternate_url']
         employer = el['employer']['name']
-        #  ))
         vacancy = Vacancy(title, desc, salary_from, salary_to, url, employer)
+
 
 def format_vacancies_sj(data):
-   # vac_sj = []
-   # data = self.get_vacancies()
+    # vac_sj = []
+    # data = self.get_vacancies()
     for el in data['objects']:
-        title=el['profession']
-        desc=el['candidat']
-        salary_from=el['payment_from']
-        salary_to=el['payment_to']
-        url=el['link']
-        employer=el['client']['title']
+        title = el['profession']
+        desc = el['candidat']
+        salary_from = el['payment_from']
+        salary_to = el['payment_to']
+        url = el['link']
+        employer = el['firm_name']
         vacancy = Vacancy(title, desc, salary_from, salary_to, url, employer)
-
-
-        # ))
-        # return vac_sj
